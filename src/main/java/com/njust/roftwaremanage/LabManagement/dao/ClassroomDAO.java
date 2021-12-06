0ab74603ef765a02bc86c009a5ceedc23c9517be
ToSnow <towintersnow@gmail.com>
2021-12-06 20:51:39 +0800

    修复了学生选课时分配位置异常的问题(详见描述)
    当有学生退选时，原先的算法可能会导致后续选课学生分配的座位号异常

---
+++ src/main/java/com/njust/roftwaremanage/LabManagement/dao/ClassroomDAO.java
@@ -0,0 +1,15 @@
+package com.njust.roftwaremanage.LabManagement.dao;
+
+import com.njust.roftwaremanage.LabManagement.entity.Classroom;
+
+public class ClassroomDAO {
+    /**
+     * 根据address找到对应的教室
+     * 输入:address(String)
+     * 输出:Classroom对象
+     * */
+    public static Classroom findClassroomByAddress(String address){
+        //TODO:
+        return null;
+    }
+}
--- src/main/java/com/njust/roftwaremanage/LabManagement/service/TableService.java
+++ src/main/java/com/njust/roftwaremanage/LabManagement/service/TableService.java
@@ -1,6 +1,7 @@
 package com.njust.roftwaremanage.LabManagement.service;
 
 import com.njust.roftwaremanage.LabManagement.dao.TableDAO;
+import com.njust.roftwaremanage.LabManagement.entity.Classroom;
 import com.njust.roftwaremanage.LabManagement.entity.Table;
 
 public class TableService {
@@ -9,32 +10,42 @@ public class TableService {
      *  输入:StudentId(String),arrangeId(String),教室名classroom(String),当前已选人数(int)
      *  输出:Table
      * */
-    public Table assignSeats(String studentId,String arrangeId, String classroom, int selectedNumber){
+    public Table assignSeats(String studentId, String arrangeId, Classroom classroom, int selectedNumber){
         //由于系统为顺序分配，为了提供效率，可以从当前已选人数开始分配位置
         int num = selectedNumber + 1;
+        int seat = assignSeats(num,classroom.getNumber_total(),classroom,arrangeId);
+        if(seat == -1){
+            //当前面有学生退选时，可能会出现获取不到座位的情况，从第一个位置继续开始，以之前的遍历位置结束
+            seat = assignSeats(1,num,classroom,arrangeId);
+        }
+        if(seat != -1){
+            //获取到了座位
+            Table table = new Table(seat, classroom.getAddress(), "正常",arrangeId,studentId);
+            return table;
+        }
+        return null;
+    }
+
+    /** 为学生分配选课时的座位（子函数）
+     *  输入:起始位置(int),结束位置(int),教室对象(Classroom),arrangeId(String)
+     *  返回:int(应该给学生分配的位置)
+     * */
+    public int assignSeats(int pos, int end, Classroom classroom, String arrangeId){
         boolean flag = false;       //是否成功分配座位
-        while(!flag && num < 200){      //避免死循环
+        while(!flag && pos <= end){      //避免超出范围
             //判断该机器是否有人
-            Table table = TableDAO.findTableByTableIdAndArrangeId(String.valueOf(num),arrangeId);
+            Table table = TableDAO.findTableByTableIdAndArrangeId(pos,arrangeId);
             if(table == null){
                 //如果该实验的座位号返回为空，则说明该位置没有人
                 //判断机器是否损坏
-                if(MachineService.checkCondition(classroom, String.valueOf(num))){
+                if(MachineService.checkCondition(classroom.getAddress(), String.valueOf(pos))){
                     //没有损坏
                     flag = true;
-                    //分配座位
-                    Table t = new Table();
-                    t.setAddress(classroom);
-                    t.setCondition("正常");
-                    t.setArrange_id(arrangeId);
-                    t.setStudent_id(studentId);
-                    //将学生的座位信息写入数据库
-                    TableDAO.InsertTable(t);
-                    return t;
+                    return pos;
                 }
             }
-            num ++;
+            pos++;
         }
-        return null;
+        return -1;
     }
 }
