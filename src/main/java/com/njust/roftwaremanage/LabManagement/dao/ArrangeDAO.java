8ad64096bc41b3788b9e1d0d0dbf1378fd922f1a
ToSnow <towintersnow@gmail.com>
2021-12-08 20:33:05 +0800

    对数据库函数进行测试，建立sql数据库
    详见readme.md

--- src/main/java/com/njust/roftwaremanage/LabManagement/dao/ArrangeDAO.java
+++ src/main/java/com/njust/roftwaremanage/LabManagement/dao/ArrangeDAO.java
@@ -6,7 +6,6 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
-import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,29 +17,25 @@ public class ArrangeDAO {
      * 根据id返回对应的实验
      * 输入:id
      * 输出:id对应的Arrange
-     * */
-    public static Arrange findArrangeById(String arrange_id){
+     */
+    public static Arrange findArrangeById(String arrange_id) {
         String resource = "mybatis-config.xml";
         SqlSession sqlSession = null;
         Arrange arrange = new Arrange();
         try {
             InputStream is = Resources.getResourceAsStream(resource);
-            if(is == null){
-                System.out.println("空");
-            }
+
             SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(is);
             sqlSession = factory.openSession();
-            arrange=sqlSession.selectOne("searcharrange", arrange_id);
+            arrange = sqlSession.selectOne("searcharrange", arrange_id);
 
             sqlSession.commit();
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             if (sqlSession != null) {
                 sqlSession.rollback();
             }
-        }
-        finally {
+        } finally {
             if (sqlSession != null) {
                 sqlSession.close();
             }
@@ -52,24 +47,26 @@ public class ArrangeDAO {
      * 根据name返回对应的实验
      * 输入:name
      * 输出:name对应的List<Arrange>
-     * */
-    public static List<Arrange> findArrangeByName(String name_exp){
-        List<Arrange> arrangeList=new ArrayList<>();
+     */
+    public static List<Arrange> findArrangeByName(String name_exp) {
+        List<Arrange> arrangeList = new ArrayList<>();
         String resource = "mybatis-config.xml";
         SqlSession sqlSession = null;
         try {
             InputStream is = Resources.getResourceAsStream(resource);
             SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(is);
             sqlSession = factory.openSession();
-            arrangeList=sqlSession.selectList("findarrangebyname",name_exp);
+            arrangeList = sqlSession.selectList("findarrangebyname", name_exp);
             sqlSession.commit();
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
-            sqlSession.rollback();
-        }
-        finally {
-            sqlSession.close();
+            if (sqlSession != null) {
+                sqlSession.rollback();
+            }
+        } finally {
+            if (sqlSession != null) {
+                sqlSession.close();
+            }
         }
         return arrangeList;
     }
@@ -77,38 +74,47 @@ public class ArrangeDAO {
     /**
      * 访问数据库，获取所有实验的名字（消除重复）
      * 输出：List<String>
-     * */
-    public static List<String> getArrangeNames(){
-        List<String> arrangeList=new ArrayList<>();
+     */
+    public static List<String> getArrangeNames() {
+        //TODO:访问数据库
+        //TODO:务必使用DISTINCT消除重复
+        List<String> arrangeList = new ArrayList<>();
         String resource = "mybatis-config.xml";
         SqlSession sqlSession = null;
         try {
             InputStream is = Resources.getResourceAsStream(resource);
             SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(is);
             sqlSession = factory.openSession();
-            arrangeList=sqlSession.selectList("getexpname");
+            arrangeList = sqlSession.selectList("getexpname");
             sqlSession.commit();
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
-            sqlSession.rollback();
-        }
-        finally {
-            sqlSession.close();
+            if (sqlSession != null) {
+                sqlSession.rollback();
+            }
+        } finally {
+            if (sqlSession != null) {
+                sqlSession.close();
+            }
         }
         return arrangeList;
     }
 
-
-    public static void main(String args[]){
+    public static void main(String[] args) {
+        System.out.println("------------------------");
+        System.out.println("根据id查找");
         Arrange arrange = ArrangeDAO.findArrangeById("1");
-        System.out.println(arrange.getArrange_id());
-        List<Arrange> arrangeList = ArrangeDAO.findArrangeByName("测试实验");
-        for(Arrange a:arrangeList){
+        System.out.println(arrange.getAddress());
+        System.out.println("------------------------");
+        System.out.println("根据实验名查找：");
+        List<Arrange> arrangeList = ArrangeDAO.findArrangeByName("hh");
+        for (Arrange a : arrangeList) {
             System.out.println(a.getArrange_id());
         }
+        System.out.println("------------------------");
+        System.out.println("获取所有实验名");
         List<String> strings = ArrangeDAO.getArrangeNames();
-        for(String s:strings){
+        for (String s : strings) {
             System.out.println(s);
         }
     }
--- src/main/java/com/njust/roftwaremanage/LabManagement/dao/ClassroomDAO.java
+++ src/main/java/com/njust/roftwaremanage/LabManagement/dao/ClassroomDAO.java
@@ -1,15 +1,49 @@
 package com.njust.roftwaremanage.LabManagement.dao;
 
 import com.njust.roftwaremanage.LabManagement.entity.Classroom;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.InputStream;
 
 public class ClassroomDAO {
     /**
      * 根据address找到对应的教室
      * 输入:address(String)
      * 输出:Classroom对象
-     * */
-    public static Classroom findClassroomByAddress(String address){
-        //TODO:
-        return null;
+     */
+    public static Classroom findClassroomByAddress(String address) {
+        String resource = "mybatis-config.xml";
+        SqlSession sqlSession = null;
+        Classroom classroom = new Classroom();
+        try {
+            InputStream is = Resources.getResourceAsStream(resource);
+            SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(is);
+            sqlSession = factory.openSession();
+            classroom = sqlSession.selectOne("findClassroomByAddress", address);
+
+            sqlSession.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (sqlSession != null) {
+                sqlSession.rollback();
+            }
+        } finally {
+            if (sqlSession != null) {
+                sqlSession.close();
+            }
+        }
+        return classroom;
+    }
+
+    public static void main(String[] args) {
+        ClassroomDAO c = new ClassroomDAO();
+        System.out.println("------------------------");
+        System.out.println("根据房间名查找房间：");
+        Classroom c2 = c.findClassroomByAddress("ss");
+        System.out.println(c2.getManager_id());
+
     }
 }
