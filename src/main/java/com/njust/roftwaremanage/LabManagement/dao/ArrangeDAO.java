5a453c7c7e7c29d353c17e0ab4807430063580bc
ToSnow <towintersnow@gmail.com>
2021-12-08 21:33:29 +0800

    增加教师排课功能(存在bug)

--- src/main/java/com/njust/roftwaremanage/LabManagement/dao/ArrangeDAO.java
+++ src/main/java/com/njust/roftwaremanage/LabManagement/dao/ArrangeDAO.java
@@ -9,6 +9,7 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class ArrangeDAO {
 
@@ -100,6 +101,96 @@ public class ArrangeDAO {
         return arrangeList;
     }
 
+    /**
+     * 增加实验安排
+     * 输入：Arrange对象
+     **/
+    public static void addArrange(Arrange arrange) {
+        String resource = "mybatis-config.xml";
+        SqlSession sqlSession = null;
+        try {
+            InputStream is = Resources.getResourceAsStream(resource);
+            if (is == null) {
+                System.out.println("空");
+            }
+            SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(is);
+            sqlSession = factory.openSession();
+            sqlSession.insert("addarrange", arrange);
+
+            sqlSession.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (sqlSession != null) {
+                sqlSession.rollback();
+            }
+        } finally {
+            if (sqlSession != null) {
+                sqlSession.close();
+            }
+        }
+    }
+
+    /**
+     * 根据starttime and endtime 返回对应的实验
+     * 输入:starttime and endtime
+     * 输出:time对应的List<Arrange>
+     * */
+    public static List<Arrange> findArrangeBytime(String starttime ,String endtime){
+        List<Arrange> arrangeList=new ArrayList<>();
+        String resource = "mybatis-config.xml";
+        Map<String,Object> parameters=new java.util.HashMap<>();
+        parameters.put("startime",Integer.parseInt(starttime));
+        parameters.put("endtime",Integer.parseInt(endtime));
+        SqlSession sqlSession = null;
+        try {
+            InputStream is = Resources.getResourceAsStream(resource);
+            SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(is);
+            sqlSession = factory.openSession();
+            arrangeList=sqlSession.selectList("findarrangebytime",parameters);
+            sqlSession.commit();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            sqlSession.rollback();
+        }
+        finally {
+            sqlSession.close();
+        }
+        return arrangeList;
+    }
+
+    /**
+     * 根据name返回对应的实验
+     * 输入:name
+     * 输出:name对应的List<Arrange>
+     * */
+    public static List<Arrange> findArrangeAllinoneday(String week,String day){
+        List<Arrange> arrangeList=new ArrayList<>();
+        String resource = "mybatis-config.xml";
+        SqlSession sqlSession = null;
+        Map<String,Object> parameters=new java.util.HashMap<>();
+        parameters.put("week",Integer.parseInt(week));
+        parameters.put("day",Integer.parseInt(day));
+//        for (String s : parameters.keySet()) {
+//            System.out.println("key:" + s);
+//            System.out.println("values:" + parameters.get(s));
+//        }
+        try {
+            InputStream is = Resources.getResourceAsStream(resource);
+            SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(is);
+            sqlSession = factory.openSession();
+            arrangeList=sqlSession.selectList("findarrangeAllinoneday",parameters);
+            sqlSession.commit();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            sqlSession.rollback();
+        }
+        finally {
+            sqlSession.close();
+        }
+        return arrangeList;
+    }
     public static void main(String[] args) {
         System.out.println("------------------------");
         System.out.println("根据id查找");
